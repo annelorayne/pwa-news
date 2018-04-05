3b429cecc15cba22ba3e854972f3a2a83e0601b5
Your Name <anne.moreira@virtus.ufcg.edu.br>
2018-04-04 23:06:31 -0300

    Adicionando o servive worker na aplicacao

---
+++ sw-pwa-news.js
@@ -0,0 +1,72 @@
+(function () {
+    'use strict'
+
+    var CACHE_SHELL = 'pwa-news-shell-v5';
+    var CACHE_DATA = 'pwa-news-data-v1';
+
+    var API = 'https://newsapi.org/v2/';
+    var FILES_SHELL = [
+        '/',
+        '/css/core.css',
+        '/css/main.css',
+        '/js/api.js',
+        '/image/placeholder-image.png',
+        '/library/jquery-3.3.1.min.js',
+        '/library/moment.min.js',
+    ];
+
+
+    self.addEventListener('install', function (event) {
+        event.waitUntil(
+            self.caches.open(CACHE_SHELL).then(function (cache) {
+                    return cache.addAll(FILES_SHELL);
+                })
+            )        
+                
+    });
+
+    self.addEventListener('activate', function (event) {
+        var cacheList = [CACHE_SHELL, CACHE_DATA];
+        console.log("SW - Activate");
+        return event.waitUntil(
+            self.caches.keys().then(function (cacheNames) {
+                return Promise.all(cacheNames.map(function name(cacheName) {
+                    if (cacheList.indexOf(cacheName) === -1) {
+                        self.caches.delete(cacheName);
+                    }
+                }))
+
+            })
+        )
+    });
+
+    self.addEventListener('fetch', function (event) {
+        if (event.request.url.indexOf(API) === -1) {
+            event.respondWith(
+                caches.match(event.request)
+                    .then(function (response) {
+                        if (response) {
+                            return response;
+                        }
+                        return fetch(event.request);
+                    })
+            )
+        } else {
+            event.respondWith(
+                // using network first 
+                self.fetch(event.request)
+                    .then(function (response) {
+                        return caches.open(CACHE_DATA)
+                            .then(function (cache) {
+                                cache.put(event.request.url, response.clone());
+                                return response;
+                            });
+                    }).catch(function (error) {
+
+                        return caches.match(event.request);
+                    })
+            )
+        }
+    });
+
+}());
